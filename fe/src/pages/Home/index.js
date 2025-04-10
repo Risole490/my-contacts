78d0c380c24ab8a5d8fcb5a10dba21ff7e3889be
Debyan <71907948+Risole490@users.noreply.github.com>
2025-04-10 09:57:45 -0300

    Form e pages Edit e NewContact desenvolvidas

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,10 +1,10 @@
+import { Link } from 'react-router-dom';
+
 import { Container, InputSearchContainer, Header, ListContainer, Card } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-
-
 
 export default function Home() {
  return (
@@ -15,7 +15,7 @@ export default function Home() {
 
       <Header>
         <strong>3 contatos</strong>
-        <a href="/">Novo contato</a>
+        <Link to="/new">Novo contato</Link>
       </Header>
 
       <ListContainer>
@@ -38,43 +38,9 @@ export default function Home() {
           </div>
 
           <div className="actions">
-            <a href="/"><img src={edit} alt="Edit" /></a>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Nome do contato</strong>
-              <small>Instagram</small>
-            </div>
-
-            <span>leozera@yan.com.br</span>
-            <span>(11) 91234-5678</span>
-          </div>
-
-          <div className="actions">
-            <a href="/"><img src={edit} alt="Edit" /></a>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Nome do contato</strong>
-              <small>Instagram</small>
-            </div>
-
-            <span>leozera@yan.com.br</span>
-            <span>(11) 91234-5678</span>
-          </div>
-
-          <div className="actions">
-            <a href="/"><img src={edit} alt="Edit" /></a>
+            <Link to="/edit/123">
+              <img src={edit} alt="Edit" />
+            </Link>
             <button type="button">
               <img src={trash} alt="Delete" />
             </button>
