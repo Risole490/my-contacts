cfe6db7f78aa342c5cb883f64981a7270cef41bf
Debyan <71907948+Risole490@users.noreply.github.com>
2025-05-28 19:04:34 -0300

    Toats sendo implementados

---
+++ fe/src/components/Toast/ToastContainer/index.js
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+export default function ToastContainer() {
+  return (
+    <Container>
+      {/* Toasts will be rendered here */}
+      <h1>ToastContainer</h1>
+    </Container>
+  );
+}
---
+++ fe/src/components/Toast/ToastContainer/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: fixed;
+  z-index: 2;
+  bottom: 48px;
+  background: red;
+  left: 50%; // Não utilizei display: flex para centralizar, pois o flexbox não é necessário aqui.
+  transform: translateX(-50%); // Centraliza o container horizontalmente pegando o tamanho total do componente. O left 50% move o container para a metade da largura da tela, e o translateX(-50%) ajusta para que ele fique centralizado.
+`;
