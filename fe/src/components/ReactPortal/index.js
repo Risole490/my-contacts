e0e4e9be78ec5832d0e430b060c6bb0763fd0471
Debyan <71907948+Risole490@users.noreply.github.com>
2025-06-15 19:59:58 -0300

    Criação de reactPortals otimizada

---
+++ fe/src/components/ReactPortal/index.js
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
+
+// Este componente ReactPortal é um exemplo de como criar um portal
+// que permite renderizar componentes React fora da hierarquia DOM pai.
+
+export default function ReactPortal({ containerId, children}) {
+  let container = document.getElementById(containerId);
+
+  if (!container) {
+    container = document.createElement("div"); // Cria um novo elemento div
+    container.setAttribute("id", containerId); // Define o ID do elemento
+    document.body.appendChild(container); // Adiciona o elemento ao body do documento
+  }
+
+  return ReactDOM.createPortal(
+    children,
+    container // Renderiza os filhos dentro do container especificado
+  );
+}
+
+ReactPortal.propTypes = {
+  containerId: PropTypes.string.isRequired,
+};
+
+ReactPortal.defaultProps = {
+  containerId: "portal-root", // ID padrão para o portal
+};
